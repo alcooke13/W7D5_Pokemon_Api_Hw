12ab53a93c32aefbaf263816d28b8515c7ece17f
Alistair <alistair13_cooke@hotmail.com>
2022-12-04 18:27:19 +0000

    mvp complete

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -5,19 +5,30 @@ import './PokemonList.css'
 const PokemonList = () => {
     const [pokemons, setPokemons] = useState([]);
     const [filter, setFilter] = useState("");
+    // const [sortedPokemons, setSortedPokemons] = useState([]);
 
     const pokemonData = "https://pokeapi.co/api/v2/pokemon?limit=150";
     fetch(pokemonData)
     .then(response => response.json())
-    .then(pokemonList => setPokemons(pokemonList.results))
-
+    .then(data => setPokemons(data.results))
+    
     const handleFilterChange = (event) => {
         setFilter(event.target.value.toUpperCase())
     }
 
+    const handleSortChange = () => {
+        console.log(pokemons)
+        const sortedPokemons = [...pokemons].sort((a,b) => {
+            return a.name> b.name ? 1: -1
+        })
+        setPokemons(sortedPokemons);
+        console.log(sortedPokemons)
+    }
+    
     return (
         <>
-        <input type="text" placeholder="Search" onChange={handleFilterChange} />
+        <input type="text" placeholder="Search" onChange={handleFilterChange} id="searchBar"/>
+        <input type="button" value="Sort A-Z" id="sort-button" onClick={handleSortChange}/>
         <section className="list-section" >
            <div className="pokemon-list-box">{pokemons.filter((pokemon) => {
             return filter === '' ? pokemon : pokemon.name.toUpperCase().includes(filter)
